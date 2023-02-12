0778dbc7035ed0ff973caebbf80e7c81c8c192b2
AnnaPenkinaPython <annapenkina200121@mail.ru>
2023-02-12 23:19:04 +0700

    tests changed1

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 @pytest.fixture()
 def test_url():
-    return "https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1676135891827&signature=W9_7uEqZfuxYQWCuBIDgYyq6Zc_fBIYq5IvFC92UVMg&downloadName=operations.json"
+    return "https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1676304439812&signature=aemOGzaGcs8NK59HigTXoIaY5zzUrNaR1h_H7O7TbUQ&downloadName=operations.json"
 
 @pytest.fixture()
 def test_data():
